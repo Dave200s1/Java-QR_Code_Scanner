7f2dd4d6be0021a75116bd64e7f0eebcc518a7fb
david <david.stefanov@web.de>
2023-04-06 13:56:39 +0200

    2023-changes

---
+++ QR_Reader_2023/src/qr_reader_2023/RunJframe.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package qr_reader_2023;
+
+/**
+ *
+ * @author David_Stefanov_Azubi
+ */
+public class RunJframe {
+    public static void main(String[]args)
+    {
+        ReadQR_Code tab = new ReadQR
+    }
+}
